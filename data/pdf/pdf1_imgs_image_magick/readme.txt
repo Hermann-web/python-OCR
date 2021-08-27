dc022888a2f74f105d76f51dc57888f6ec05afac
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2021-08-27 19:44:41 +0100

    change pdf_to_img

---
+++ data/pdf/pdf1_imgs_image_magick/readme.txt
@@ -0,0 +1,3 @@
+in cmd:: magick convert EDP-Etudiants.pdf pdf1.jpg
+#ca marche car imageMagick est installe, ghostscript aussi
+
--- pdf_to_img.py
+++ pdf_to_img.py
@@ -64,7 +64,7 @@ def get_image_folder(pdf_path,add=''):
 
 
 def pdf_to_img(filepath):
-    return way2(filepath)
+    return way3(filepath)
 
 if __name__ == '__main__':
     filename = "data/pdf/EDP-etudiants.pdf"
